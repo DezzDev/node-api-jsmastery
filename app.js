b2f1250fc2f452a12bcc66548eadef4bc5d08bea
daniel <daniel-dezz@hotmail.com>
2025-03-17 14:19:36 +0100

    Add error handling middleware and configure app with cookie parser and body parsing

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDataBase from './database/mongoDb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 import { PORT } from './config/env.js';
+
 
 const app = express();
 
+// middleware
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded, 
+// help us to process the data from the form
+app.use(express.urlencoded({ extended: true }));
+// parse cookies from the request headers and populate req.cookies with an object keyed by the cookie names. 
+// this need be imported before the routes
+app.use(cookieParser())
+// error middleware
+app.use(errorMiddleware)
+
+// routes
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
